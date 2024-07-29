cebfd2fc9f01357ec5fff4be546cb558ae93e0de
Harshavignyan <ahvignyan@gmail.com>
2024-07-29 12:33:52 +0530

    final multer

--- multer/server.js
+++ multer/server.js
@@ -1,18 +1,37 @@
 const express = require('express')
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
-
+var fs = require("fs")
+var bodyParser = require('body-parser')
 const app = express()
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public/index.html"))
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/public/index.html")
 })
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-    // req.file is the `avatar` file
-    // req.body will hold the text fields, if there were any
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        const path = `uploads/${req.body.username}/`
+        if (!fs.existsSync(path)) {
+            file.path = path
+            fs.mkdirSync(path)
+            callback(null, path)
+          }
+        else{
+            file.path = path
+            callback(null,path)
+        }
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/profile', upload.single('avatar'), (req,res) => {
     console.log(req.file, req.body)
     res.send("file and body received")
 })
