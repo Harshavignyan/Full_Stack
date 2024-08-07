809490d0cc4b24f641465247b62f66ba64578f7d
Harsha Vignyan Ayaluri <52147540+Harshavignyan@users.noreply.github.com>
2024-08-07 09:59:26 +0530

    sdx

--- websocketsbasic/routes/employee/employee.routes.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const { getAllEmployees } = require('./employee.controller');
-var router = express.Router();
-
-router.get("/allEmployees",getAllEmployees)
-
-
-module.exports=router;
